b997aa237aeca2efa92b47eb21d55afe85d6f3b5
Peter Schueler <peter@orangemirrorentertainment.com>
2018-06-08 15:08:01 +0200

    Added Basic Tab & iFrame Support

--- javascript/functions.js
+++ javascript/functions.js
@@ -3,3 +3,19 @@ function toggleDropdown(element, parent) {
 	var men = document.getElementById(parent);
 	men.classList.toggle('radius');
 }
+
+function toggleTab(event, type) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace("active", "");
+	}
+	document.getElementById(type).style.display = "block";
+	evt.currentTarget.className += " active";
+}
+
+document.getElementById('defaultOpen').click();
